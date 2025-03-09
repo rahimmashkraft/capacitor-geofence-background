65df58d19d2a7c3e0fea9ab915ecaf236cf6f805
rahim <rahim@sdsol.com>
2025-03-09 16:40:37 +0500

    Fix android issues

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { registerPlugin } from '@capacitor/core';
 export interface GeofenceBackgroundPlugin {
   startLocationService(): Promise<void>;
   stopLocationService(): Promise<void>;
+  checkAndRequestPermissions(): Promise<void>;
 }
 
 const GeofenceBackground = registerPlugin<GeofenceBackgroundPlugin>('GeofenceBackground');
